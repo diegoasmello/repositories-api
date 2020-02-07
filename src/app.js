357d6ed5517e8cd5a5c1c78fab058893356c99a5
Diego Mello <diegoasmello@gmail.com>
2020-02-07 02:27:36 -0300

    api routes

--- src/app.js
+++ src/app.js
@@ -4,13 +4,57 @@ const request = require("request");
 const app = express();
 const port = 3001;
 
-app.all("*", (req, res, next) => {
+const API_URL = "https://api.github.com";
+
+app.all("*", (_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
+});
+
+app.get("/search/:term", (req, res, next) => {
+  const options = {
+    url: `${API_URL}/search/repositories?q=${req.params.term}`,
+    headers: {
+      "User-Agent": "request",
+    },
+  };
+
+  request.get(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send({ status: true, data: JSON.parse(body) });
+      next();
+    } else {
+      res.status(200).send({ status: false, data: "" });
+      next();
+    }
+  });
+});
+
+app.get("/repository/:user/:repo", (req, res, next) => {
+  try {
+    const options = {
+      url: `${API_URL}/users/${req.params.user}/repos`,
+      headers: {
+        "User-Agent": "request",
+      },
+    };
+
+    request.get(options, (error, response, body) => {
+      if (error && response.statusCode != 200) {
+        return;
+      }
+
+      res.status(200).send({ status: true, data: JSON.parse(body) });
+      next();
+    });
+  } catch (e) {
+    res.status(500).send({ status: false, data: e.message });
+    next();
+  }
 });
 
 app.listen(port, (err) => {
